db89c8f1a99cfe5a620af3d28245c7f503c72e1d
Faheem Ahmad <faheem.ahmad@pb.com>
2023-05-18 14:11:18 +0530

    changes
    changes

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+data "aws_availability_zones" "available" {}
+
+locals{
+  azs      = slice(data.aws_availability_zones.available.names, 0, var.az_no)
+  public_subnets_cidr = (var.public_subnets == []) ? [for k, v in local.azs : cidrsubnet(var.vpc_cidr_block, 4, k)] : var.public_subnets
+  private_subnets_cidr = (var.private_subnets == []) ? [for k, v in local.azs : cidrsubnet(var.vpc_cidr_block, 4, k + 4 )] : var.private_subnets
+}
+
+
 resource "aws_vpc" "vpc" {
   cidr_block       = var.vpc_cidr_block
   instance_tenancy = "default"
