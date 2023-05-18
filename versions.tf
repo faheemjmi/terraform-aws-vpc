503e6f598f654fc8337022fab58caeea20c2aa8e
Faheem Ahmad <faheem.ahmad@pb.com>
2023-05-18 17:05:25 +0530

    changes

--- versions.tf
+++ versions.tf
@@ -1,10 +1,9 @@
-# Terraform Block
 terraform {
-  required_version = ">= 1.0"
+  required_version = "~> 1.2.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 4.5.0"
     }
   }
 }
