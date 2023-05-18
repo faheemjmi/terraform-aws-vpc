8cfca76e450b259bc5b47552a635a088ef9a9cdc
Faheem Ahmad <faheem.ahmad@pb.com>
2023-05-18 14:34:33 +0530

    vpc endpoint
    vpc endpoint

--- variables.tf
+++ variables.tf
@@ -1,14 +1,28 @@
 # VPC Input Variables
-variable "aws_region" {}
+variable "aws_region" {
+    type = string
+    default = "us-east-1"
+ }
 # VPC Name
-variable "vpc_name" {}
+variable "vpc_name" {
+    type = string
+    default = ""
+}
+
+variable "vpc_cidr_block" {
+  description = "(Optional) The IPv4 CIDR block for the VPC. CIDR can be explicitly set or it can be derived from IPAM using `ipv4_netmask_length` & `ipv4_ipam_pool_id`"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
 variable az_no {
+    type = number
     default = 3
 }
 variable  "use_single_nat" {
      default = false
 }
-variable "vpc_cidr_block" {}
+
 variable  "use_tgw" {
     default = "no"
 }
