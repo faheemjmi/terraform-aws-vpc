2fb71dea5474d5734c8b6e72d9c4b3f3a70663b9
Faheem Ahmad <faheem.ahmad@pb.com>
2023-05-19 12:12:08 +0530

    vpc variables

--- variables.tf
+++ variables.tf
@@ -8,13 +8,11 @@ variable "aws_region" {
 variable "vpc_name" {
     description = "vpc name"
     type = string
-    default = ""
 }
 
 variable "vpc_cidr_block" {
   description = "(Optional) The IPv4 CIDR block for the VPC. CIDR can be explicitly set or it can be derived from IPAM using `ipv4_netmask_length` & `ipv4_ipam_pool_id`"
   type        = string
-  default     = "10.0.0.0/16"
 }
 
 variable az_no {
